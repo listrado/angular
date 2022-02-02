0b82c5319973327666d57719ec9db832301c380b
listrado <llucas.cardi.rangel@gmail.com>
2022-02-02 16:33:59 -0300

    timer and alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
   providers: [],
   bootstrap: [AppComponent]
